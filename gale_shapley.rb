49d7e4c1dc5509d049426c437207ccb3bc32a807
Raphael Esterle <raphael.esterle@gmail.com>
2014-06-10 01:29:33 +0200

    Visualisation finisched

--- gale_shapley.rb
+++ gale_shapley.rb
@@ -83,11 +83,11 @@ class Task
   
   def is_free? obj
     @pairs ||= {}
-    !@pairs.has_value? obj
+    not @pairs.has_value? obj
   end
 
   def higher_ranked? obj, rank
-    return true if !@pairs.has_key?obj
+    return true unless @pairs.has_key?obj
     @model.group(1)[obj].each do |place|
       return true if place==rank
       return false if place==@pairs[obj]
@@ -123,48 +123,77 @@ class Visualize
   
   def model= model
     @model = model
-    puts "model: #{model}"
+  end
+  
+  def pairs= pairs
+    @pairs = pairs
+  end
+
+  def search_node name, group
+    n = nil
+    @graphs.each do |k, v|
+       if k != group
+         n = v.search_node name.to_s unless n
+       end
+    end
+    n
+  end 
+
+  def create_edges
+    @model.each do |group, obj|
+      obj.each do |name, likes|
+        n0 = @graphs[group].get_node name.to_s
+        c = 1
+        likes.each do |like|
+          color = 'black'
+          color = 'green' if @pairs[like] == name
+          n1 = search_node like, group
+          @graphs[group].add_edge(n0, n1, {:color => color, :label => c.to_s})
+          c +=1
+        end
+      end
+    end
   end
 
   def create_image name
     g = GraphViz.new(:G, :type => :digraph)
     @model.each do |k, v|
-      puts "k: #{k} v:#{v.class}"
       if v.is_a? Hash
-        v.each do |name, likes|
-          n0 = g.add_nodes(name.to_s)
-          likes.each do |l|
-            n1 = g.add_nodes(l.to_s)
-            g.add_edges(n0, n1)
-          end
+        @color = "blue" unless @color
+        @graphs ||= {}
+        @graphs[k] = g.add_graph(k.to_s, {:label => k.to_s, :color => @color.dup})
+        v.each_key do |name|
+          @graphs[k].add_node(name.to_s, :color => @color.dup)
         end
+        @color = "red"
       else
-        n0 = g.add_nodes(k.to_s)
-        n1 = g.add_nodes(v.to_s)
+        n0 = g.add_node(k.to_s)
+        n1 = g.add_node(v.to_s)
         g.add_edges(n0, n1)
       end
     end
+    create_edges if @graphs
     g.output( :png => "#{name}.png")
   end
 end
 
 if __FILE__ == $0
-  if ARGV.size>1
+  if ARGV.size>2
     puts 'Usage: match <model.rb> <output.png>'
     exit 1
   end
+  file = ARGV.shift
   model = Model.new
-  model.instance_eval(File.open(ARGV.shift).read)
-
-  v = Visualize.new
-  v.model = model.objects
-  v.create_image 'likes'
+  model.instance_eval(File.open(file).read)
 
   task = Task.new
   task.model = model
   task.stable_matching
-
-  puts "stable: #{task.result}"
-  v.model = task.result
-  v.create_image 'result'
+  
+  model2 = Model.new
+  model2.instance_eval(File.open(file).read)
+  v = Visualize.new
+  v.model = model2.objects
+  v.pairs = task.result
+  v.create_image ARGV.shift
 end
